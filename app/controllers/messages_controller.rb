9539e51b5a76b1aa9ccab7a723aa0e414e33e493
mizupi <mizukif2206@gmail.com>
2022-02-01 12:32:09 +0900

    チャットルーム名の表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,15 +2,17 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @room = Room.find(params[:room_id])
+    @messages = @room.messages.includes(:user)
   end
-
+  
   def create
     @room = Room.find(params[:room_id])
-    @message =@room.messages.new(room_params)
+    @message = @room.messages.new(room_params)
     @message.save
     if @message.save
       redirect_to room_messages_path(@room)
     else
+      @messages = @room.messages.includes(:user)
       render :index
     end
       
